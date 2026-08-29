11cb23e564ae6dd56948f97755cf36cbb35031a6
Leinura <fashonai026@gmail.com>
2026-08-29 16:46:34 +0530

    loirehe

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -14,6 +14,7 @@ export const colors = {
   statusPending: "#9A9AA5",
   statusInProgress: "#EF9F27",
   statusCompleted: "#1D9E75",
+  statusReturnRepeat: "#FFD54F",
 };
 
 export const spacing = { xs: 4, sm: 8, md: 16, lg: 24, xl: 32 };
